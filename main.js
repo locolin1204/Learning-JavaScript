2c7c5a5d4cca438ff84aa0b1fbc3e18965b4a699
locolin <locolin99@gmail.com>
2022-05-25 16:41:58 +0800

    update worker js

--- main.js
+++ main.js
@@ -1,100 +1,25 @@
-// setup canvas
+const fetchPromise1 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+const fetchPromise2 = fetch('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found');
+const fetchPromise3 = fetch('bad-scheme://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json');
 
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
-
-// function to generate random number
-
-function random(min, max) {
-  const num = Math.floor(Math.random() * (max - min + 1)) + min;
-  return num;
-}
-
-// function to generate random color
-
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
-}
-
-class Ball{
-  constructor(x, y, velX, velY, color, size){
-    this.x = x;
-    this.y = y;
-    this.velX = velX;
-    this.velY = velY;
-    this.color = color;
-    this.size = size;
-  }
-  draw(){
-    ctx.beginPath();
-    ctx.fillStyle = this.color;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.fill();
-  }
-
-  update(){
-    if((this.x + this.size >= width)){
-      this.velX = -this.velX;
+Promise.any([fetchPromise1, fetchPromise2, fetchPromise3])
+  .then( responses => {
+    for (const response of responses) {
+      console.log(`${response.url}: ${response.status}`);
     }
-    if((this.x - this.size <= 0)){
-      this.velX = -this.velX;
-    }
-    if((this.y + this.size >= height)){
-      this.velY = -this.velY;
-    }
-    if((this.y - this.size <= 0)){
-      this.velY = -this.velY;
-    }
-    this.x += this.velX;
-    this.y += this.velY;
-  }
+  })
+  .catch( error => {
+    console.error(`Failed to fetch: ${error}`)
+  });
 
-  collisionDetect(){
-    for(const ball of balls){
-      if(!(this === ball)){
-        const dx = this.x - ball.x;
-        const dy = this.y - ball.y;
-        const distance = Math.sqrt(dx * dx + dy * dy);
   
-        if(distance < this.size + ball.size){
-          ball.color = this.color = randomRGB();
-        }
+  function alarm(person, delay) {
+    return new Promise((resolve, reject) => {
+      if (delay < 0) {
+        throw new Error('Alarm delay must not be negative');
       }
-    }
+      window.setTimeout(() => {
+        resolve(`Wake up, ${person}!`);
+      }, delay);
+    });
   }
-
-}
-
-
-const balls = [];
-
-while(balls.length < 25){
-  const size = random(10,20);
-  const ball = new Ball(
-    random(0 + size, width - size),
-    random(0 + size, height - size),
-    random(-7,7),
-    random(-7,7),
-    randomRGB(),
-    size
-  );
-  balls.push(ball);
-}
-
-function loop(){
-  ctx.fillStyle = "rgba(0,0,0,0.25)";
-  ctx.fillRect(0, 0, width, height);
-
-  for(const ball of balls){
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
-  }
-  requestAnimationFrame(loop);
-}
-
-loop();
-
